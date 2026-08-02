678d0ec4fca012779c1056415eccdcf7ed7c1472
wenv <lmiriam521@gmail>
2026-08-01 20:32:17 -0400

    terraform1.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,3 @@
-#checkov:skip=CKV_AWS_18: "Bucket logging no requerido en lab"
-#checkov:skip=CKV_AWS_144: "Replicacion no requerida en lab"
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -15,22 +12,6 @@ resource "aws_s3_bucket_public_access_block" "publico" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "cifrado" {
-  bucket = aws_s3_bucket.bucket_seguro.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_versioning" "versionado" {
-  bucket = aws_s3_bucket.bucket_seguro.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
 
 resource "aws_security_group" "sg_seguro" {
